db8eb100f9e6975f5ccf8092ced41e0f8eec8d87
spansare <spansari@in.ibm.com>
2017-02-02 18:23:12 +0530

    Update LoanServiceImpl.java

--- src/main/java/com/capg/loanservice/service/impl/LoanServiceImpl.java
+++ src/main/java/com/capg/loanservice/service/impl/LoanServiceImpl.java
@@ -3,6 +3,7 @@ package com.capg.loanservice.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.capg.loanservice.dao.BranchDao;
