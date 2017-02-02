0ca6fac6716198f6bde778755960dfd506801b1e
spansare <spansari@in.ibm.com>
2017-02-02 18:10:54 +0530

    Update Application.java

--- src/main/java/com/capg/loanservice/Application.java
+++ src/main/java/com/capg/loanservice/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.apache.log4j.Logger;
 
 @SpringBootApplication
+@ComponentScan("com.capg.loanservice")
 public class Application   {
 	
     private static Logger log = Logger.getLogger(Application.class);
