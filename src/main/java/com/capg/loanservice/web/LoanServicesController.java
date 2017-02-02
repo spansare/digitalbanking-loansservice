6d1c8a47a44982a412e61a7292b6a782855f39d9
spansare <spansari@in.ibm.com>
2017-02-02 18:29:13 +0530

    Update LoanServicesController.java

--- src/main/java/com/capg/loanservice/web/LoanServicesController.java
+++ src/main/java/com/capg/loanservice/web/LoanServicesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import com.capg.loanservice.model.Customer;
 import com.capg.loanservice.model.Loan;
