928c00d860f532c5bd0a0e3dda9146c9ba90fd19
spansare <spansari@in.ibm.com>
2017-02-02 18:15:06 +0530

    Update LoanService.java

--- src/main/java/com/capg/loanservice/service/LoanService.java
+++ src/main/java/com/capg/loanservice/service/LoanService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.capg.loanservice.model.Branch;
 import com.capg.loanservice.model.Customer;
 import com.capg.loanservice.model.Loan;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface LoanService {
 	
 	public List<Loan> getLoans(Customer customer);
